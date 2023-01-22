9a09fd651d1b9c53ee03fffaf12c2fd61a73c3f9
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-21 21:07:19 -0700

    Add project files.

---
+++ Mission6/Models/AddMovie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission6.Models
+{
+    public class AddMovie
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Category is required")]
+        string Category;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required")]
+        string Title;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Year is required")]
+        int Year;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Director is required")]
+        string Director;
+
+        bool Edited;
+
+        string LentTo;
+
+        [StringLength(25)]
+        string Notes;
+    }
+}
