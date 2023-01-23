2eb9bee0e5c03ceb70d2674d855f22a9cff50b75
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-22 20:31:21 -0700

    Ready for seed

--- Mission6/Models/MoviesContext.cs
+++ Mission6/Models/MoviesContext.cs
@@ -18,10 +18,51 @@ namespace Mission6.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<AddMovie>().HasData(
-                    new AddMovie
-                    {
-
-                    }
+                new AddMovie
+                {
+                    Id = 1,
+                    Category = "Action/Adventure",
+                    Title = "Top Gun",
+                    Year = "1986",
+                    Director = "Tony Scott",
+                    Rating = "PG"
+                },
+                new AddMovie
+                {
+                    Id = 2,
+                    Category = "Comedy",
+                    Title = "The Sandlot",
+                    Year = "1993",
+                    Director = "David Mickey Evans",
+                    Rating = "PG"
+                },
+                new AddMovie
+                {
+                    Id = 3,
+                    Category = "Television",
+                    Title = "The Office",
+                    Year = "2005-2013",
+                    Director = "Various",
+                    Rating = "PG"
+                },
+                new AddMovie
+                {
+                    Id = 4,
+                    Category = "Comedy",
+                    Title = "Grown Ups",
+                    Year = "2010",
+                    Director = "Dennis Dugan",
+                    Rating = "PG-13"
+                },
+                new AddMovie
+                {
+                    Id = 5,
+                    Category = "Action/Adventure",
+                    Title = "Spider-Man",
+                    Year = "2002",
+                    Director = "Sam Raimi",
+                    Rating = "PG-13"
+                }
             );
         }
     }
