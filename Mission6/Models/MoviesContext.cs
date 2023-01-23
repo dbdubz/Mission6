d96a8b74e7a25ff747d3c6ec0c3b0ddc50e276fd
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-22 18:29:27 -0700

    Create Context

---
+++ Mission6/Models/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options) : base (options)
+        {
+            // Leave blank for now
+        }
+
+        public DbSet<AddMovie> addMovies { get; set; }
+    }
+}
