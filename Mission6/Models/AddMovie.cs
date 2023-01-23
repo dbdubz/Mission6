6b7074d1c29dda4699d1ddaee5127de966c14b98
Dalyn Baldwin <dlbaldwi@byu.edu>
2023-01-22 18:21:05 -0700

    Added Connection String and dependencies

--- Mission6/Models/AddMovie.cs
+++ Mission6/Models/AddMovie.cs
@@ -9,22 +9,22 @@ namespace Mission6.Models
     public class AddMovie
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Category is required")]
-        string Category;
+        public string Category;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required")]
-        string Title;
+        public string Title;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Year is required")]
-        string Year;
+        public string Year;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Director is required")]
-        string Director;
+        public string Director;
 
-        bool Edited;
+        public bool Edited;
 
-        string LentTo;
+        public string LentTo;
 
         [StringLength(25)]
-        string Notes;
+        public string Notes;
     }
 }
